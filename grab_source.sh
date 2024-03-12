77e4c9509eca74d5e2a8172804afff43932a3ae6
Tom Wallis <github@tomwallis.net>
2024-03-12 11:52:24 +0000

    Update to chromium 122.0.6261.69 (currently testing)

--- grab_source.sh
+++ grab_source.sh
@@ -1,4 +1,4 @@
-wget https://commondatastorage.googleapis.com/chromium-browser-official/chromium-121.0.6167.160.tar.xz
-tar -xf chromium-121.0.6167.160.tar.xz --strip-components 1
+wget https://commondatastorage.googleapis.com/chromium-browser-official/chromium-122.0.6261.69.tar.xz
+tar -xf chromium-122.0.6261.69.tar.xz --strip-components 1
 git restore .gitignore readme.md
-rm chromium-121.0.6167.160.tar.xz
+rm chromium-122.0.6261.69.tar.xz
