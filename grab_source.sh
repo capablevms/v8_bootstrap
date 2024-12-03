2493a4b371f600cba34f874e2f3b2db163c53932
Tom Wallis <github@tomwallis.net>
2024-12-03 12:12:57 +0000

    Update V8 to 12.6.228.21 from probablytom/v8

--- grab_source.sh
+++ grab_source.sh
@@ -1,4 +1,4 @@
-wget https://commondatastorage.googleapis.com/chromium-browser-official/chromium-122.0.6261.69.tar.xz
-tar -xf chromium-122.0.6261.69.tar.xz --strip-components 1
+wget https://commondatastorage.googleapis.com/chromium-browser-official/chromium-126.0.6478.127.tar.xz
+tar -xf chromium-126.0.6478.127.tar.xz --strip-components 1
 git restore .gitignore readme.md
-rm chromium-122.0.6261.69.tar.xz
+rm chromium-126.0.6478.127.tar.xz
